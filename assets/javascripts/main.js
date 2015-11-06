2d69fb93767031ce1195d3aabb946c9b1155eab9
AndrewDubovtsev <andrey.dubovtsev@gmail.com>
2015-11-07 02:54:49 +0300

    Initial gallery

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -28,32 +28,56 @@ function sliderInitialise() {
   );*/
 }
 
+function owlGallery() {
+  $(".owl-item").find(".small-img").removeClass("current");
+  var $this = $(".owl-item.active").eq(1).find(".small-img");
+  $this.addClass("current");
+  var imgRefString = $this.attr("src");
+  $this.parents(".gallery").find(".big-img").attr("src",imgRefString);
+  if ($this.hasClass("vertical")) {
+    $this.parents(".gallery").find(".big-img").addClass("vertical");
+  } else {
+    $this.parents(".gallery").find(".big-img").removeClass("vertical");
+  }
+}
+
+function addActiveToImage($this) {
+  $(".small-img").removeClass("current");
+  if ($this === ".small-img") {
+    $this.addClass("current");
+  } else {
+    $this.find(".small-img").addClass("current");
+  }
+  var imgRefString = $this.attr("src");
+  $this.parents(".gallery").find(".big-img").attr("src",imgRefString);
+  if ($this.hasClass("vertical")) {
+    $this.parents(".gallery").find(".big-img").addClass("vertical");
+  } else {
+    $this.parents(".gallery").find(".big-img").removeClass("vertical");
+  }
+}
+
 function openGallery() {
-  $(".gallery").find(".small-img").on("click",function() {
-    $(".small-img").removeClass("active");
-    $(this).addClass("active");
-    var imgRefString = $(this).attr("src");
-    $(this).parents(".gallery").find(".big-img").attr("src",imgRefString);
-    if ($(this).hasClass("vertical")) {
-      $(this).parents(".gallery").find(".big-img").addClass("vertical");
-    } else {
-      $(this).parents(".gallery").find(".big-img").removeClass("vertical");
-    }
-  });
-  $(".gallery").each(function() {
-    var galleryLength = 0;
-    var marginWidth = 5;
-    var newMargin = marginWidth - 1;
-    $(this).find("li").each(function() {
-      galleryLength += $(this).width();
-    });
-    galleryLength += ($(this).find("li").length - 1) * marginWidth;
-    if (galleryLength > $(this).width()) {
-      $(this).find("li").css("margin-right",newMargin + "px");
-    }
+  $(document).on("click",".small-img",function() {
+    var $this = $(this);
+    addActiveToImage($this);
   });
 }
 
+function getLastCurrentSlide(currentSlide) {
+ // console.log(currentSlide);
+ // var photoName = "big"; //photoName contains "big" and number, for ex. big11
+//  var photoNameLastSymPosition = $this.lastIndexOf(photoName) + photoName.length;
+  //var currentSlideNumber = $this.slice(photoNameLastSymPosition);
+  //currentSlideNumber = currentSlideNumber.slice(0,currentSlideNumber.lastIndexOf("."));
+
+ // var currentSlideNumber = currentSlide.index();
+//  e.preventDefault();
+  //console.log(currentSlideNumber);
+  //carousel.to(carousel.relative($this.index()));
+ // addActiveToImage($(currentSlide));
+  return currentSlide.index() + 1;
+}
 
 $(function() {
 
@@ -71,13 +95,6 @@ $(function() {
    });
   }
 
-  if ($(".gallery").length) {
-    openGallery();
-    $(window).resize(function() {
-      openGallery();
-    });
-  }
-
   if ($("#lightgallery").length) {
     var $lg = $("#lightgallery");
     $lg.lightGallery({
@@ -86,13 +103,41 @@ $(function() {
       autoplay: true
       //showThumbByDefault: true
     });
-   /* $lg.on('onBeforeOpen.lg',function(event){
-      $(".lg-outer .lg-thumb-item").css("width","auto");
-      $(".lg-outer .lg-thumb-item").html("yo");
-    });*/
   }
 
   if ($(".owl-carousel").length) {
-    $(".owl-carousel").owlCarousel();
+    var owl = $(".owl-carousel");
+    owl.each(function () {
+      var currentSlide = 0;
+      var currentSlideNumber = 0;
+      $(this).owlCarousel({
+        margin: 5,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        autoWidth: true,
+        stopOnHover: true,
+        autoplayHoverPause: true,
+        loop: true,
+        items: 10
+      }).on("changed.owl.carousel", function () {
+        owlGallery();
+      })
+        .parents(".gallery").hover(function () {
+          console.log("gallery");
+        //  owl.trigger("stop.owl.autoplay");
+          openGallery();
+        }, function () {
+          currentSlide = $(this).find(".current").parents(".owl-item");
+            //.attr("src");
+          currentSlideNumber = getLastCurrentSlide(currentSlide);
+          console.log(currentSlideNumber);
+          console.log("gallery out");
+          owl
+            .trigger("to.owl.carousel",currentSlideNumber);
+          addActiveToImage($(currentSlide));
+            //.trigger("stop.owl.autoplay");
+           // .trigger("play.owl.autoplay");
+        });
+    });
   }
 });
